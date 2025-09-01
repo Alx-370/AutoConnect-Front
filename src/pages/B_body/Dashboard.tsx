2fcb8c0053726ef9fbce8f99e1e3d5a8de26e92d
Alex <alexismoreau37320@gmail.com>
2025-09-01 12:20:01 +0200

    pîlou check

--- src/pages/B_body/Dashboard.tsx
+++ src/pages/B_body/Dashboard.tsx
@@ -1,11 +1,13 @@
 
 import Header from "../A_header/Header";
+import Footer from "../C_footer/Footer.tsx";
 
 const Dashboard = () => {
     return (
         <>
             <Header />
             <div style={{ padding: 16 }}>Je suis dans le Dashboard de l'accueil</div>
+            <Footer />
         </>
     );
 };
