66dbf625be9316d3385361ea0ee3aef1b59085d8
Alex <alexismoreau37320@gmail.com>
2025-09-09 16:41:21 +0200

    maj QuoteCard pour recupe info locastorage venant de searchgarage

--- src/types/quote.ts
+++ src/types/quote.ts
@@ -1,14 +1,26 @@
+export type ServiceItem = { id: number; name: string; price: number };
+
 export type QuoteLS = {
+
     immat?: string;
     km?: string;
     make?: string | null;
     model?: string | null;
     year?: number | string | null;
-    services?: Array<number | string>;
-};
+    services?: number[];
 
-export type ServiceItem = {
-    id: number;
-    name: string;
-    price: number;
+
+    id?: number;
+    name?: string;
+    typeVoie?: string;
+    libelleVoie?: string;
+    codePostal?: string;
+    libelleCommune?: string;
+    phoneNumber?: string;
+    appointment?: { start: string; end: string };
+    totalHT?: number;
+    tva?: number;
+    totalTTC?: number;
+    quoteNumber?: string;
+    validatedAt?: string;
 };
