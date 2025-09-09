5f176adf285663c8a166e549b0c4dd4a1e9bfca0
Alex <alexismoreau37320@gmail.com>
2025-09-09 11:11:14 +0200

    maj des axiosCar

--- src/api/axiosCar.ts
+++ src/api/axiosCar.ts
@@ -2,21 +2,22 @@ import axios from "axios";
 import type { MakeDto, ModelDto, YearDto } from "../types/car";
 
 const API_BASE = "http://localhost:8080";
+const BASE_END_POINT = "api/car";
 
 export async function fetchMakes(): Promise<MakeDto[]> {
-    const { data } = await axios.get<MakeDto[]>(`${API_BASE}/api/car/mark`);
+    const { data } = await axios.get<MakeDto[]>(`${API_BASE}/${BASE_END_POINT}/mark`);
     return data;
 }
 
 export async function fetchModels(make: string): Promise<ModelDto[]> {
-    const { data } = await axios.get<ModelDto[]>(`${API_BASE}/api/car/model`, {
+    const { data } = await axios.get<ModelDto[]>(`${API_BASE}/${BASE_END_POINT}/model`, {
         params: { make },
     });
     return data;
 }
 
 export async function fetchYears(make: string, model: string): Promise<YearDto[]> {
-    const { data } = await axios.get<YearDto[]>(`${API_BASE}/api/car/year`, {
+    const { data } = await axios.get<YearDto[]>(`${API_BASE}/${BASE_END_POINT}/year`, {
         params: { make, model },
     });
     return data;
